b8f9a9ed5d505ee5e9a6366c899118b6033a7845
Porter, Andrew <Andrew.Porter@bottomline.com>
2019-07-09 12:29:11 -0400

    Fixed regex to exclude numbers and did work towards analysis

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import csv
 import re
 
+import pandas as pd
 import wikipedia
 
 one_hot_file = "one_hot_encoding.csv"
 
-def prune(content):
-    """Removes words from reference sections at the bottom of an article"""
-    return content
+def get_content(num_pages=2):
+    """Retrieves page contents from random Wikipedia articles"""
+    all_words = set()
+    all_content = []
+
+    for i in range(0, num_pages):
+        content = wikipedia.page(wikipedia.random(pages=1)).content
+        words = re.findall(r"\d*[a-zA-Z]+\d*[a-zA-Z]+\d*", content)  # extract only words, not numbers
+        all_content.append(content)
+        all_words.update(words)
+    
+    return all_words, all_content
 
 
 def build_one_hot_encoding(all_words, all_content):
@@ -18,7 +28,7 @@ def build_one_hot_encoding(all_words, all_content):
         writer.writerow(all_words)
 
         for content in all_content:
-            content_words = re.findall(r"\w+", content)
+            content_words = re.findall(r"\d*[a-zA-Z]+\d*[a-zA-Z]+\d*", content)  # extract only words, not numbers
             content_words = set(content_words)
 
             content_one_hot = []
@@ -30,20 +40,18 @@ def build_one_hot_encoding(all_words, all_content):
             writer.writerow(content_one_hot)
                 
 
+def analyze():
+    """Runs analysis on the generated one-hot encoding file"""
+    df = pd.read_csv(one_hot_file)
+    print(df.head())
+    print(df.mean())
+
+
 def run():
     """Build a stoplist from contents"""
-    all_words = set()
-    all_content = []
-    for i in range(0, 5):
-        content = wikipedia.page(wikipedia.random(pages=1)).content
-        content = prune(content)
-
-        words = re.findall(r"\w+", content)
-
-        all_content.append(content)
-        all_words.update(words)
-
+    all_words, all_content = get_content()
     build_one_hot_encoding(all_words, all_content)
+    analyze()
 
 
 if __name__ == "__main__":
